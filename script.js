fbcc3282c5ec0b460fcf7a83833060e7e3994ed3
Pallab dutta <121430112+pallab1999@users.noreply.github.com>
2025-03-17 19:32:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading spaces first
+    str = str.trim();
+    
+    // Find the index of the first space
+    let spaceIndex = str.indexOf(' ');
+    
+    // If no space is found, return the entire string
+    return spaceIndex === -1 ? str : str.slice(0, spaceIndex);
 }
 
 // Do not change the code below
